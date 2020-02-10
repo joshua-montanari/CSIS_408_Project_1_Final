e4283c8da46e4d3ffe96eadbb0fdf93ab9b0d892
joshua-montanari <joshua.montanari@gmail.com>
2020-02-09 22:24:04 -0500

    bug fixes, difficulty modes

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+//HTML Canvas Calls
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+//Ball
 var ballRadius = 10;
 var x = canvas.width / 2;
 var y = canvas.height - 20;
 var dx = 3;
 var dy = 3;
+//Paddle
 var paddleHeight = 10;
-var paddleWidth = 75;
+var paddleWidth = 95;
 var paddleX = (canvas.width - paddleWidth) / 2;
+//Keyboard
 var rightPressed = false;
 var leftPressed = false;
-
-var lives= 5;
+//Score and Lives
+var lives= 6;
 var score = 0;
-
+//Brick Field
 var brickRowCount = 5;
 var brickColumnCount = 9;
 var brickWidth = 80;
@@ -23,18 +27,32 @@ var brickOffsetTop = 20;
 var brickOffsetLeft = 20;
 
 //change difficulty
-function difficultySelect(rowCount, columnCount, padding, width){
+var difficulty = prompt("Select Difficulty: Easy, Medium, Hard");
+
+if (difficulty == 'Medium' || difficulty == 'medium'){
+    document.getElementById("diff").innerHTML = 'Medium Mode'
+    difficultySelect(3, 5, 10, 135, 40)
+}else if (difficulty == 'Hard' || difficulty == 'hard'){
+    document.getElementById("diff").innerHTML = 'Hard Mode'
+    difficultySelect(5, 9, 5, 80, 20);
+}else{
+    document.getElementById("diff").innerHTML = 'Easy Mode'
+    difficultySelect(1, 5, 10, 135, 40)
+}
+
+//changes brick field depending on mode
+function difficultySelect(rowCount, columnCount, padding, width, offet){
     brickRowCount = rowCount;
     brickColumnCount = columnCount;
     brickWidth = width;
     brickPadding = padding;
+    brickOffsetLeft = offet;
     console.log('function is called')
     console.log(brickRowCount)
     return brickRowCount, brickColumnCount, brickWidth, brickPadding
 }
 
-console.log(brickRowCount)
-
+//loads array of bricks
 var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
@@ -43,10 +61,12 @@ for (var c = 0; c < brickColumnCount; c++) {
     }
 }
 
+//keyboard and mouse movement listeners
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
+//Keyboard functions
 function keyDownHandler(e) {
     if (e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = true;
@@ -65,6 +85,7 @@ function keyUpHandler(e) {
     }
 }
 
+//mouse function
 function mouseMoveHandler(e) {
     var relativeX = e.clientX - canvas.offsetLeft;
     if(relativeX > 0 && relativeX < canvas.width) {
@@ -72,36 +93,27 @@ function mouseMoveHandler(e) {
     }
 }
 
-
+//Keeps Track of users lives
 function playerLives() {
-   
     if (lives !== 1){
-          lives = lives - 1;
+        lives = lives - 1;
         document.getElementById("life").innerHTML = "Lives: " + lives; 
-        
-    }
-    
-    else { 
+    }else { 
         document.getElementById("life").innerHTML = "Lives: " + lives; 
-        //window.alert("Game Over");
         window.location.replace("lose.html");
-        
     }
-   
 }
 
-//brickColumnCount * brickRowCount)* 100
-
+//keeps track of users score
 function playerScore(){
     score = score + 100;
     if(score >= (brickColumnCount * brickRowCount)* 100){
-        //document.querySelector('winLives') = "Lives Left: " + lives;
-        //document.querySelector('winScore') = "Score: " + score;
         window.location.replace("win.html");
     }
     document.getElementById("score").innerHTML = "Score: " + score;
 }
 
+//sees if bricks are hit and then adjusts the objects
 function collisionDetection() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
@@ -111,15 +123,13 @@ function collisionDetection() {
                     dy = -dy;
                     b.status = 0;
                     playerScore();
-            
                 }
             }
         }
     }
 }
 
-
-
+//Creates the ball
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -128,16 +138,15 @@ function drawBall() {
     ctx.closePath();
 }
 
+//resets the ball after a life is lost
 function ballReset(){
     playerLives();
     dx = -dx;
-	y = canvas.width/3;
-    x = canvas.height/2;
-   
+	y = canvas.width/4;
+    x = canvas.height/3;
 }
 
-
-
+//Creates the paddle
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -146,6 +155,7 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+//fills the brick field and colors it
 function drawBricks() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
@@ -182,9 +192,7 @@ function drawBricks() {
     }
 }
 
-
-
-
+//Creates all elements
 function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
@@ -199,29 +207,24 @@ function draw() {
     }
     if (y + dy < ballRadius) {
         dy = -dy;
-    }
-    else if (y + dy > canvas.height - ballRadius) {
+    }else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             if (y = y - paddleHeight) {
                 dy = -dy;
             }
-        }
-        else {
+        }else {
            ballReset();
-            //document.location.reload();
-            //clearInterval(interval); // Needed for Chrome to end game
         }
     }
 
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
-    }
-    else if (leftPressed && paddleX > 0) {
+    }else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
     }
-
     x += dx;
     y += dy;
 }
 
+//draws the components on an interval
 var interval = setInterval(draw, 10);
